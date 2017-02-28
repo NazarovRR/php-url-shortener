83aadb218f0c50dcbf5a18658b0a8ee7493b1f41
Mike <nazarovmichaelrr@gmail.com>
2017-02-28 11:25:38 +0300

    fix for redirect check

--- application/helpers/MY_url_helper.php
+++ application/helpers/MY_url_helper.php
@@ -2,7 +2,7 @@
 function webpage_exist($webpage)
 {
 	$headers = @get_headers($webpage);
-	if(in_array("HTTP/1.0 200 OK", $headers)) {
+	if(is_array($headers) && in_array("HTTP/1.0 200 OK", $headers)) {
 		return TRUE;
 	} else {
 		return FALSE;
